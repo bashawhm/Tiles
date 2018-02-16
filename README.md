b6bf2792abb02858f2ed1d38e4e23abc0194c148
Hunter <bashawhm@clarkson.edu>
2018-02-16 17:29:30 -0500

    Changed Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tiles
 A simple game in SDL
+
+# Compiling
+Install SDL2, SDL2_image, SDL2_mixer.
+Run build.sh
--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 #include "stage.h"
 
+
 int main(){
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window * window = SDL_CreateWindow("Tiles", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
